216f95c230a71cd57c2988571c0b8dad9e0b9798
stefan-leo-vegas <stefan.lachmann@leovegas.com>
2018-03-20 10:07:36 +0100

    fixed index.css and included Tachyons lib

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import './index.css';
 
 class App extends Component {
   render() {
