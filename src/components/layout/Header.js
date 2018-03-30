fe82118f2912739da84e691c1b12eb871972b585
Stefan Lachmann <stefanlachmann@hotmail.com>
2018-03-30 16:25:52 +0200

    component updates

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
-import {Link, BrowserRouter} from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 
 // TODO switch componentent onclick with react-router and delete contentCard component
+
+const about = () => <span>ABOUT</span>;
+const experience = () => <span>ex</span>;
+const project = () => <span>Project</span>;
+const skills = () => <div className="tabContent"></div>;
+const contact = () => <div className="tabContent">Contact</div>;
+
+
 class Header extends Component {
+    
     render() {
+
+        const { path } = this.props.match;
+
         return (
-            <div>
+
+          <div>
                 <nav className="nav header">
                     <ul className="nav-bar">
-                        <li className="nav-bar_item f4 lh-copy"><a rel="noopener noreferrer" href="/">About</a></li>
-                        <li className="nav-bar_item f4 lh-copy"><a>Experience</a></li>
+                        <li className="nav-bar_item f4 lh-copy"><Link to={`${path}`} className="link">About</Link></li>
+                        <Link to={`${path}`} className="link"><li className="nav-bar_item f4 lh-copy"><a>Experience</a></li></Link>
                         <li className="nav-bar_item f4 lh-copy"><a href="/">Project</a></li>
                         <li className="nav-bar_item f4 lh-copy"><a href="/">Skills</a></li>
-                        <li className="nav-bar_item f4 lh-copy nav-bar_item--last"><a>Contact</a></li>
+                        <Link to={`${path}/contact`} className="link">Contact</Link>
                     </ul>
                 </nav>
+                <div className="tabs">
+                    <Switch>
+                        <Route path={"/about"} exact component={about} />
+                        <Route path={"/experience"} exact component={experience} />
+                        <Route path={'/contact'} component={contact} />
+                    </Switch>
+                </div>
             </div>
         );
     }
 
 }
 export default Header;
+
--- src/components/topBanner/index.js
+++ src/components/topBanner/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './style.css';
 
+
 function Banner(props) {
     if (!props.warn) {
         return null;
@@ -11,31 +12,77 @@ function Banner(props) {
     </div>
     );
 }
+// TODO build a checker to see if i clicked it away before  
+function cookieCheck() {
+    let checker = document.cookie.indexOf('Acookie=');
+    let hasCookie;
+    console.log(checker)
 
-class Topbanner extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { showWarning: true };
-        this.handleToogleClick = this.handleToogleClick.bind(this);
-    }
-    handleToogleClick(){
-        this.setState(prevState => ({
-            showWarning: !prevState.showWarning
-        }));
+    var cookie = 'Acookie=';
+    var cookies = cookie.split('; ');
+    cookies.forEach(function(c){
+        if(c.match(/Acookie=.+/)){
+         console.log(true);
+        }else{ 
+            console.log(false);
+        }
+
+      });
+
+    if (checker === -1) {
+        hasCookie = true;
+    }else{
+        hasCookie =  false;
+        }
+    return hasCookie;
     }
 
-    render() {
-        return (
-            <div>
-            <Banner warn={this.state.showWarning} />
-            <div className="hideButton f4 fw6 db black link dim" onClick={this.handleToogleClick}>
-                {this.state.showWarning ? 'X': ''}
-            </div>
-            </div>
-        )
+    class Topbanner extends Component {
+        constructor(props) {
+            super(props);
+            console.log(cookieCheck())
+            // (cookieCheck)
+            this.state = { showWarning: true };
+            this.handleToogleClick = this.handleToogleClick.bind(this);
+        }
+        handleToogleClick() {
+            this.setState(prevState => ({
+                showWarning: !prevState.showWarning
+            }));
+
+
+            document.cookie = [
+                'you', 1,
+                'have', 2,
+                'been', 3,
+                'before', 4
+            ]
+
+          /*  function setCookie(exdays) {
+                var d = new Date();
+                d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+                var expires = "expires=" + d.toUTCString();
+                document.cookie = 'CookieN1ame' + "=" + 'Cookie-value' + ";" + `expires: ${expires}` + ";path=/";
+
+                const aCookie = ['you', 1, 'have', 2, 'been', 3, 'before', 4, [1, 'says', 2, 'I']];
+                document.cookie = 'Acookie' + '=' + aCookie;
+            }
+            setCookie(); */
+
+        }
+
+        render() {
+            return (
+                <div>
+                    <Banner warn={this.state.showWarning} />
+                    <div className="hideButton f4 fw6 db black link dim" onClick={this.handleToogleClick}>
+                        {this.state.showWarning ? 'X' : ''}
+                    </div>
+                </div>
+            )
+        }
     }
-}
-export default Topbanner;
+    export default Topbanner;
 
 
 /*
