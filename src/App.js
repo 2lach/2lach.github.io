038dfbc03c822a22e35250cd800a966bc5729720
Stefan Lachmann <stefanlachmann@hotmail.com>
2018-03-30 16:25:22 +0200

    new routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
 import React, { Component } from 'react';
 import './index.css';
-
-import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
-import Clock from './components/clock/index';
-import ContentCard from './components/contentCard/index';
-//import Logincontrol from './components/loginControl/index';
+import Clock from './components/Clock/index';
+// import ContentCard from './components/ContentCard/index';
+// import Header from './components/layout/Header';
+// import Logincontrol from './components/loginControl/index';
 import Topbanner from './components/topBanner/index';
+import { Switch, Route, Link } from 'react-router-dom';
+
+
+const about = () => <div className="tabContent border-me">ABOUT</div>;
+const experience = () => <div className="tabContent border-me">Experience</div>;
+const Project = () => <div className="tabContent border-me">Project</div>;
+const Skills = () => <div className="tabContent border-me">Skills</div>;
+const Contact = () => <div className="tabContent border-me">Contact</div>;
 
 
 
 class App extends Component {
   render() {
+
+    const { path } = this.props.match;
+
     return (
       <div className="App">
-        <Clock/>
-        <Topbanner  /> 
-        <Header className="fl w-third" />
-        <ContentCard className="fl w-third"/>    
+        <Clock />
+        <Topbanner />
+        <div>
+          <nav className="nav header">
+            <ul className="nav-bar">
+              <Link className="nav-bar_item f4 lh-copy" to={`${path}/About`}>About</Link>
+              <Link className="nav-bar_item f4 lh-copy" to={`${path}/Experience`} >Experience</Link>
+              <Link className="nav-bar_item f4 lh-copy" to={`${path}/Project`}>Project</Link>
+              <Link className="nav-bar_item f4 lh-copy" to={`${path}/Skills`}>Skills</Link>
+              <Link className="nav-bar_item f4 lh-copy" to={`${path}/Contact`}>Contact</Link>
+            </ul>
+          </nav>
+          <div className="tabs">
+            <Switch>
+              <Route path={"/home/about"} exact component={about} />
+              <Route path={"/home/experience"} exact component={experience} />
+              <Route path={'/home/contact'} component={Contact} />
+              <Route path={`/home/project`}  component={Project} />
+              <Route path={'/home/Skills'} component={Skills} />
+              <Route path={'/home/contact'} component={Contact} />
+            </Switch>
+          </div>
+        </div>
+
         <Footer className="fl w-100" />
-        {/*  <Logincontrol />     */}
+        {/*  <Logincontrol />     <Header />          <ContentCard className="fl w-third" /> */}
       </div>
-      
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// Routing
+import { BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Routes = (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/home" component={App} />
+        <Redirect from="/" to="/home" />
+      </Switch>
+    </BrowserRouter>
+);
+
+
+
+ReactDOM.render(Routes, document.getElementById('root'));
 registerServiceWorker();
