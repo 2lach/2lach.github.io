ae3bba7aee4afafb7efb272152cda6c662e8b41b
Stefan Lachmann <stefanlachmann@hotmail.com>
2018-08-31 14:13:50 +0200

    phone-fix

--- src/components/Links.js
+++ src/components/Links.js
@@ -14,12 +14,12 @@ class Links extends Component{
     return(
             <div className="flexRow">
                 <ul style={{listStyleType: 'none', display: 'contents'}}>
-                    <li className="item"><a href="https://github.com/2lach" rel="noopener"><IoLogoGithub style={{width:40,height:50}} /><br /> Github</a></li>
-                    <li className="item"><a href="https://www.linkedin.com/in/slachmann/" rel="noopener"><IoLogoLinkedin style={{width:40,height:50}}/><br /> Linkedin</a></li>
-                    <li className="item"><a href="https://codepen.io/2lach/" rel="noopener"><IoLogoCodepen style={{width:40,height:50}}/><br /> Codepen</a></li>
-                    <li className="item"><a href="https://twitter.com/2lach__" rel="noopener"><IoLogoTwitter style={{width:40,height:50}}/><br /> Twitter</a></li>
-                    <li className="item"><a href="https://medium.com/@2lach" rel="noopener"><FaMedium style={{width:40,height:50}}/><br /> Medium</a></li>
-                    <li className="item" onClick={this.openAlert}><a rel="noopener"><IoMdContact style={{width:40,height:50}}/><br /> Contact</a></li>
+                    <li className="item"><a href="https://github.com/2lach" rel="noopener"><IoLogoGithub style={{width:40,height:50}} /><br /><span>Github</span> </a></li>
+                    <li className="item"><a href="https://www.linkedin.com/in/slachmann/" rel="noopener"><IoLogoLinkedin style={{width:40,height:50}}/><br /> <span>Linkedin</span></a></li>
+                    <li className="item"><a href="https://codepen.io/2lach/" rel="noopener"><IoLogoCodepen style={{width:40,height:50}}/><br /> <span>Codepen</span></a></li>
+                    <li className="item"><a href="https://twitter.com/2lach__" rel="noopener"><IoLogoTwitter style={{width:40,height:50}}/><br /> <span>Twitter</span></a></li>
+                    <li className="item"><a href="https://medium.com/@2lach" rel="noopener"><FaMedium style={{width:40,height:50}}/><br /> <span>Medium</span></a></li>
+                    <li className="item" onClick={this.openAlert}><a rel="noopener"><IoMdContact style={{width:40,height:50}}/><br /><span>Contact</span> </a></li>
                 </ul>
             </div>
 )
