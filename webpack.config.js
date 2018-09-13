a192f361a9d62bcf94bb62c09a7fd5f490d4c2ef
Stefan Lachmann <stefanlachmann@hotmail.com>
2018-09-13 23:32:49 +0200

    replaced webpack friendly errs with webpack bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
   entry: ['babel-polyfill', './src/index.js'],
@@ -48,16 +48,36 @@ module.exports = {
   },
   performance: {
     hints: process.env.NODE_ENV === 'production' ? 'warning' : false,
-    maxEntrypointSize: 512000,
+    maxEntrypointSize: 51200,
     maxAssetSize: 512000,
+  },
+  devServer: {
+    quiet: true,
   },
   plugins: [
     new webpack.optimize.ModuleConcatenationPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve('template/index.ejs'),
     }),
+    new BundleAnalyzerPlugin({
+      analyzerPort: 3003,
+      openAnalyzer: false, // change to true for visual overview
+      statsFilename: './monitor.stats.json',
+      logLevel: 'info', //info, warn, error or silent 
+    }),
     new webpack.NamedModulesPlugin(),
-    // add FriendlyErrorsWebpackPlugin better error logging for build
     new FriendlyErrorsWebpackPlugin(),
   ],
 }
+
+/*
+  plugins: [
+    new WebpackMonitor({
+      capture: true, // -> default 'true'
+      target: './monitor/stats.json',
+      errors: false,
+      // launch: true,
+      port: 3030, // default -> 8081
+    }),
+    ]
+*/
