7e31650c149549a93d78f3dd8ea2453bd619175a
Stefan Lachmann <stefanlachmann@hotmail.com>
2018-09-14 10:30:02 +0200

    some webpack updates and formating in app.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,87 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const webpack = require('webpack')
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
 
 module.exports = {
-  entry: ['babel-polyfill', './src/index.js'],
+  entry: {
+    main: ["babel-polyfill", "./src/index.js"],
+    other: ["babel-polyfill", "./src/components/App.js"]
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           query: {
-            presets: ['react'],
-            plugins: ['transform-object-rest-spread'],
-          },
-        },
+            presets: ["react"],
+            plugins: ["transform-object-rest-spread"]
+          }
+        }
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.(jpeg|jpg|png|gif|svg)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              limit: 200000,
-            },
-          },
-        ],
-      },
-    ],
+              limit: 200000
+            }
+          }
+        ]
+      }
+    ]
   },
   performance: {
-    hints: process.env.NODE_ENV === 'production' ? 'warning' : false,
+    hints: process.env.NODE_ENV === "production" ? "warning" : false,
     maxEntrypointSize: 51200,
-    maxAssetSize: 512000,
+    maxAssetSize: 512000
   },
   devServer: {
-    quiet: true,
+    quiet: true
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new webpack.optimize.ModuleConcatenationPlugin(),
+    new CleanWebpackPlugin(["dist"]),
     new HtmlWebpackPlugin({
-      template: path.resolve('template/index.ejs')
+      template: path.resolve("template/index.ejs")
     }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
     new webpack.HashedModuleIdsPlugin(),
-    new webpack.NamedModulesPlugin(),
-    // Error handling and analyzer 
+    // Error handling and analyzer
     new FriendlyErrorsWebpackPlugin(),
     new BundleAnalyzerPlugin({
       analyzerPort: 3333,
       openAnalyzer: false, // change to true for visual overview
-      statsFilename: './monitor.stats.json',
-      logLevel: 'silent', //info, warn, error or silent 
-    }),
+      statsFilename: "./monitor.stats.json",
+      logLevel: "silent" //info, warn, error or silent
+    })
   ],
   output: {
-    filename: '[name].[hash].js',
-    chunkFilename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    filename: "[name].[hash].js",
+    chunkFilename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/"
   },
   optimization: {
-    runtimeChunk: 'single',
+    runtimeChunk: "single",
     splitChunks: {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
+          name: "vendors",
+          minChunks: Infinity,
+          chunks: "all"
         }
       }
-    },
-  },
-}
+    }
+  }
+};
