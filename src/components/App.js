e9c602f4af2d8f56df4787d08956022644b49916
Stefan Lachmann <stefanlachmann@hotmail.com>
2020-01-04 22:56:20 +0100

    Updates to App.js and wrote random joke component

--- src/components/App.js
+++ src/components/App.js
@@ -6,18 +6,21 @@ import Writings from "./Writings/Writings";
 import "../styles/App.css";
 import { CookieConsent, checkConsent } from "./CookieConsent";
 import LatestCommits from "./LatestCommits/LatestCommits";
+import BadJokes from "./BadJokes";
 
 const App = () => (
   <div>
     <CoverImage />
     <Avatar />
     <div className="content">
-      <h1>My name is Stefan and I build things</h1>
+      <h1>My name is Stefan and I build things!</h1>
       <h2>Dev of the North</h2>
-      <h3>Father, builder, thinker & breaker</h3>
+      <h3>Father, builder, thinker & coder</h3>
       <Links />
     </div>
+    <BadJokes />
     <LatestCommits />
+
     {!checkConsent("cookieConsentClicked") && <CookieConsent />}
   </div>
 );
