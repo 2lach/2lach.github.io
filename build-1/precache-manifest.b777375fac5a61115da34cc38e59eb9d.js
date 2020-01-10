33245e292b7d3fa9d2a40bd469cfd4a4c475f9b0
Stefan Lachmann <stefanlachmann@hotmail.com>
2020-01-10 13:56:09 +0100

    build 1 from new template

---
+++ build-1/precache-manifest.b777375fac5a61115da34cc38e59eb9d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756ba9bede8c03500b19e46e87355242",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb1cadf317188c747135",
+    "url": "/static/css/main.cb186cac.chunk.css"
+  },
+  {
+    "revision": "4a0d37c1dfaee0ed67c8",
+    "url": "/static/js/2.ccec3e51.chunk.js"
+  },
+  {
+    "revision": "6843e0c60308b05ada4a76c1b67f6fc8",
+    "url": "/static/js/2.ccec3e51.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb1cadf317188c747135",
+    "url": "/static/js/main.c533fe87.chunk.js"
+  },
+  {
+    "revision": "d69d7a275df0875bfd2e",
+    "url": "/static/js/runtime-main.d94b1e1e.js"
+  },
+  {
+    "revision": "e4b4ae8d179efe0dc3294c9c4f48e17a",
+    "url": "/static/media/landscape.e4b4ae8d.jpg"
+  },
+  {
+    "revision": "dbc9274c2c47719ccc9160b334888752",
+    "url": "/static/media/portrait.dbc9274c.jpg"
+  },
+  {
+    "revision": "71b8f5aea6bacf0a422e1c71544753ca",
+    "url": "/static/media/stefan.71b8f5ae.jpg"
+  }
+]);
