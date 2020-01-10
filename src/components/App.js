ca2ee58e84098db351f5b5ce4f59116e9b80b751
Stefan Lachmann <stefanlachmann@hotmail.com>
2020-01-10 09:18:36 +0100

    added login

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import CoverImage from "./CoverImage";
 import Avatar from "./Avatar";
 import Links from "./Links";
-import Writings from "./Writings/Writings";
+import Login from "./Login";
 import "../styles/App.css";
 import { CookieConsent, checkConsent } from "./CookieConsent";
 import LatestCommits from "./LatestCommits/LatestCommits";
 import BadJokes from "./BadJokes";
 
-const App = () => (
-  <div>
-    <CoverImage />
-    <Avatar />
-    <div className="content">
-      <h1>My name is Stefan and I build things!</h1>
-      <h2>Dev of the North</h2>
-      <h3>Father, builder, thinker & coder</h3>
-      <Links />
-    </div>
-    <BadJokes />
-    <LatestCommits />
+function App() {
+  const [knock, setKnock] = useState(0);
+  return (
+    <div>
+      <CoverImage />
+      <Avatar onClick={() => setKnock(knock + 1)} />
+      {knock > 3 && <Login />}
+      <div className="content">
+        <h1>My name is Stefan and I build things!</h1>
+        <h2>Dev of the North</h2>
+        <h3>Father, builder, thinker & coder</h3>
+        <Links />
+      </div>
+      <BadJokes />
+      <LatestCommits />
 
-    {!checkConsent("cookieConsentClicked") && <CookieConsent />}
-  </div>
-);
+      {!checkConsent("cookieConsentClicked") && <CookieConsent />}
+    </div>
+  );
+}
 export default App;
