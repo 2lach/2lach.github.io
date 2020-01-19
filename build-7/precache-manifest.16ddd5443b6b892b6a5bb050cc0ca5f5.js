b15de50f773a7d17c3e285dea42d1d3632070ab5
Stefan Lachmann <stefanlachmann@hotmail.com>
2020-01-19 21:51:48 +0100

    apply fixes, restructure and added todos | added prettier:check script, updates to stylesheets | cleanup of cache, added prettierignore file, added pkg axios
    (cherry picked from commit 52e5f97fc0b219a365ff6b726166760a207f79d4)

---
+++ build-7/precache-manifest.16ddd5443b6b892b6a5bb050cc0ca5f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c47282e3aa03585f1cb91fead3f70a81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a37d0b8173fbd5565a62",
+    "url": "/static/css/main.1528091a.chunk.css"
+  },
+  {
+    "revision": "893ba29a44bdebe5b43b",
+    "url": "/static/js/2.435ca592.chunk.js"
+  },
+  {
+    "revision": "6843e0c60308b05ada4a76c1b67f6fc8",
+    "url": "/static/js/2.435ca592.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a37d0b8173fbd5565a62",
+    "url": "/static/js/main.608f92db.chunk.js"
+  },
+  {
+    "revision": "8cb23e0d737cc4a90d59",
+    "url": "/static/js/runtime-main.f935acd5.js"
+  },
+  {
+    "revision": "e4b4ae8d179efe0dc3294c9c4f48e17a",
+    "url": "/static/media/landscape.e4b4ae8d.jpg"
+  },
+  {
+    "revision": "ed553be842c0095290fb75b0c75bc88c",
+    "url": "/static/media/portrait.ed553be8.jpg"
+  },
+  {
+    "revision": "71b8f5aea6bacf0a422e1c71544753ca",
+    "url": "/static/media/stefan.71b8f5ae.jpg"
+  }
+]);
