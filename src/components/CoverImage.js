7ce63ccd8e7352fa8ed54102b675471efd2b114b
Stefan Lachmann <slac@kth.se>
2023-06-20 12:18:22 +0200

    fix pre-load for images

--- src/components/CoverImage.js
+++ src/components/CoverImage.js
@@ -1,11 +1,9 @@
-// import Media from "react-media";
-// import ProgressiveImage from "react-progressive-graceful-image";
-import { Component } from "react";
-import landscape from "../assets/images/landscape.jpg";
-import landscapeX60 from "../assets/images/landscapeX60.jpg";
-import portrait from "../assets/images/portrait.jpg";
-import portraitX60 from "../assets/images/portraitX60.jpg";
-import styled from "styled-components";
+import { Component } from 'react';
+import landscape from '../assets/images/landscape.jpg';
+import landscapeX60 from '../assets/images/landscapeX60.jpg';
+import portrait from '../assets/images/portrait.jpg';
+import portraitX60 from '../assets/images/portraitX60.jpg';
+import styled from 'styled-components';
 
 const StyledProgressiveImage = styled.img`
   height: 65vh;
@@ -26,30 +24,40 @@ const StyledProgressiveImage = styled.img`
 class CoverImage extends Component {
   constructor(props) {
     super(props);
-    this.state = { matches: window.matchMedia("(min-width: 768px)").matches };
+    this.state = {
+      matches: window.matchMedia('(min-width: 768px)').matches,
+      mainImageLoaded: false
+    };
   }
 
   componentDidMount() {
-    const handler = (e) => this.setState({ matches: e.matches });
+    const handler = e => this.setState({ matches: e.matches });
     window
-      .matchMedia("(min-width: 1000px)")
-      .addEventListener("change", handler);
+      .matchMedia('(min-width: 1000px)')
+      .addEventListener('change', handler);
   }
+
+  handleMainImageLoad = () => {
+    this.setState({ mainImageLoaded: true });
+  };
+
   render() {
+    const { matches, mainImageLoaded } = this.state;
+
     return (
       <div data-testid='cover-image'>
-        {this.state.matches && (
+        {matches && (
           <StyledProgressiveImage
-            src={landscape}
+            src={mainImageLoaded ? landscape : landscapeX60}
             alt='the main splash'
-            placeholder={landscapeX60}
+            onLoad={this.handleMainImageLoad}
           />
         )}
-        {!this.state.matches && (
+        {!matches && (
           <StyledProgressiveImage
-            src={portrait}
+            src={mainImageLoaded ? portrait : portraitX60}
             alt='the small splash'
-            placeholder={portraitX60}
+            onLoad={this.handleMainImageLoad}
           />
         )}
       </div>
