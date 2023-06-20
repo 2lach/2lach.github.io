070dad1b2d98ceb96f0b83626e1a797e3cd1a713
Stefan Lachmann <slac@kth.se>
2023-06-20 19:49:53 +0200

    38
    (cherry picked from commit 106cc948544b3f161a3bd208086d06dbd10ec98f)

--- deploymentscript2.sh
+++ deploymentscript2.sh
@@ -1,39 +1,22 @@
-#!/bin/bash
+#
+g co development
 
- # step (y/n)
- yarn build
- # step (y/n)
- ls -la public
- # step (y/n)
- rm -rf deployment-33
- # step (y/n)
- rm -rf dist
- # step (y/n)
- ls
- # step (y/n)
- mkdir deployment-35
- # step (y/n)
- cp -rv public build deployment-35 
- # step (y/n)
- g s
- # step (y/n)
- g a
- # step (y/n)
- g s
- # step (y/n)
- g cm 'deployment 35'
- # step (y/n)
- gp
- # step (y/n)
- g llg
- # step (y/n)
- g co master
- # step (y/n)
  g co .
- # step (y/n)
- g reset HEAD
- # step (y/n)
+
+ g co development
+
+ yarn build
+
+ yarn start
+
+ yarn build
+
+ yarn start
+
+ g s
+
  g a
+<<<<<<< HEAD
  # step (y/n)
  g cm 'old build'
  # step (y/n)
@@ -81,8 +64,118 @@
  # step (y/n)
  gs
  # step (y/n)
- gp
+||||||| parent of 106cc94 (38)
+ # step (y/n)
+ g cm 'old build'
  # step (y/n)
  g s
- 
+ # step (y/n)
+ g cherry-pick -x c9884c91424b0ddc307d4d6b64b05e55238c1d60
+ # step (y/n)
+ g a
+ # step (y/n)
+ mv deployment-35/public/* .
+ # step (y/n)
+ mv static _static
+ # step (y/n)
+ mv deployment-35/build/android-chrome-192x192.png
+ # step (y/n)
+ mv deployment-35/build/* .
+ # step (y/n)
+ rm deployment-35
+ # step (y/n)
+ g s
+ # step (y/n)
+ g a
+ # step (y/n)
+ g reset _static
+ # step (y/n)
+ g s
+ # step (y/n)
+ echo 'LATEST BUILDNR: 35' > version.txt
+ # step (y/n)
+ g s
+ # step (y/n)
+ git cherry-pick --continue 
+ # step (y/n)
+ g s
+ # step (y/n)
+ g a
+ # step (y/n)
+ g reset _static
+ # step (y/n)
+ g s
+ # step (y/n)
+ g cm 'version update'
+ # step (y/n)
+ g log
+ # step (y/n)
+ gs
+ # step (y/n)
+=======
+
+ g cm 'update cache policy and serve images in defined size'
+
+>>>>>>> 106cc94 (38)
+ gp
+
+ yarn build
+
+ ls
+
+ ls -la public
+
+ mkdir deployment-37
+
+ cp -rv public build deployment-37
+
+ g s
+
+ g a
+
+ echo 'LATEST BUILDNR: 37' > version.txt
+
+ g a
+
+ g cm 'deployment 37'
+
+ gp
+
+ g ll
+
+ g llg
+
+ g s
+
  g co master
+
+ g s
+
+ g a
+
+ g cm 'add build'
+
+ g cherry-pick -x 48ecea2533b2ea31185f39c2f8a30f4a8f869dda
+
+ g s
+
+ mv deployment-36/build/* .
+
+ mv deployment-37/build/* .
+
+ mv static _static
+
+ mv deployment-37/build/* .
+
+ rd deployment-37/build
+
+ mv deployment-37/public/* .
+
+ g s
+
+ g a
+
+ g cm 'deployment 37'
+
+ gp
+ g s
