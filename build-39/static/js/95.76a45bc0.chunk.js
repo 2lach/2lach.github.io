0bc4b8920825862ab67045cd9453c5b65b9645c6
Stefan Lachmann <stefanlachmann@hotmail.com>
2025-01-02 16:21:36 +0100

    Build 39

---
+++ build-39/static/js/95.76a45bc0.chunk.js
@@ -0,0 +1,21 @@
+"use strict";(self.webpackChunk_2lach_github=self.webpackChunk_2lach_github||[]).push([[95],{278:(e,t,r)=>{r.r(t),r.d(t,{default:()=>c});const a=r.p+"static/media/sl.ca23e199c5e44f82e48d.jpg";var i=r(403),o=r(579);const l=i.Ay.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-content: center;
+  align-items: center;
+  margin-top: -125px;
+`,s=i.Ay.img`
+  background-size: cover;
+  border: 2px solid white;
+  border-top-left-radius: 0;
+  border-top-right-radius: 15%;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 10%;
+  order: 0;
+  flex: 0 1 auto;
+  align-self: auto;
+  box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.35);
+`,c=()=>(0,o.jsx)(l,{children:(0,o.jsx)(s,{fetchpriority:"high",src:a,height:"250",width:"248",placeholder:a,alt:"2lach avatar"})})}}]);
+//# sourceMappingURL=95.76a45bc0.chunk.js.map
