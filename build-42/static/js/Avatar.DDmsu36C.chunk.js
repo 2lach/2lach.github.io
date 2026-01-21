51a22827727fcebd3c1a0fa47182bf50e04b8096
Stefan Lachmann <stefanlachmann@hotmail.com>
2026-01-21 21:45:32 +0100

    build 42

---
+++ build-42/static/js/Avatar.DDmsu36C.chunk.js
@@ -0,0 +1,20 @@
+import{j as t}from"./index.CaLdzb6D.js";import{a}from"./styled-components.browser.esm.C_IyVmTt.chunk.js";const r="/static/media/sl.Bc-EGpE9.jpg",e=a.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-content: center;
+  align-items: center;
+  margin-top: -125px;
+`,o=a.img`
+  background-size: cover;
+  border: 2px solid white;
+  border-top-left-radius: 0;
+  border-top-right-radius: 15%;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 10%;
+  order: 0;
+  flex: 0 1 auto;
+  align-self: auto;
+  box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.35);
+`,n=()=>t.jsx(e,{children:t.jsx(o,{src:r,height:"250",width:"248",placeholder:r,alt:"2lach avatar"})});export{n as default};
